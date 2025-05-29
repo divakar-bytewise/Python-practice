da30bde4b72ee6949a713c87e5d8d2a9bd8bb630
diakar-bytewise <divakar.c@diggibyte.com>
2025-05-29 18:40:53 +0530

    Functions done

---
+++ Functions/F1.py
@@ -0,0 +1,44 @@
+#Calling the function
+def v_happy(name):
+    print(f"Happy birthday {name}")
+v_happy('stev')
+v_happy('josh')
+
+#With arguments.
+def arth(a,b):
+    print(a+b)
+    print(a//b)
+    print(a/b)
+    print(a-b)
+    print(a*b)
+arth(7,4)
+
+#Arbitrary arguments.
+def v_happy(*name):
+    print("Happy birthday",name[1])
+
+v_happy('stev','john','dominic')
+
+#Keyword Function
+def v_happy(f1,f2,f3):
+    print("Happy birthday",f3)
+
+v_happy(f1='stev',f2='john',f3='dominic')
+
+#Function with return value.
+def expo(a,b):
+    r=a**b
+    return r
+
+r=expo(7,4)
+print(r)
+
+#Passing list on function.
+def names_list(n):
+    for i in n:
+        print(i)
+names=('stev','john','dominic')
+names_list(names)
+
+
+    
---
+++ Functions/F2_recursion.py
@@ -0,0 +1,7 @@
+def num_count(n):
+    if n==77:
+        return
+    print("The number is : ",n)
+    num_count(n+1)
+
+num_count(0)
