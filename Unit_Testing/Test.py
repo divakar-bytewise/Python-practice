7e564c03a63a3f9af1c8e62ac7648fbd179a1af1
diakar-bytewise <divakar.c@diggibyte.com>
2025-06-03 19:25:49 +0530

    Unit_Testing is done.

---
+++ Unit_Testing/Test.py
@@ -0,0 +1,16 @@
+import unittest
+from Function import multiply
+
+class TestMultiplyFunction(unittest.TestCase):
+    
+    def test_multiply_positive(self):
+        self.assertEqual(multiply(2, 3),6)
+        
+    def test_multiply_negative(self):
+        self.assertEqual(multiply(-2, -2), 4)
+        
+    def test_multiply_zero(self):
+        self.assertEqual(multiply(0, 5), 0)
+
+if __name__ == '__main__':
+    unittest.main()
