4920af6674d195206e81bff3524c49fff8daef96
diakar-bytewise <divakar.c@diggibyte.com>
2025-06-12 19:38:12 +0530

    implemented advance topics like map, reduce, flatmap, lambda, generator, and filter.

---
+++ src/advance_topics/flatmap.py
@@ -0,0 +1,11 @@
+from itertools import chain
+
+lists = [[1, 2], [3, 4], [5]]
+flat_map= chain.from_iterable(map(lambda x: x, lists))
+print(list(flat_map))
+
+#Using List Compherension.
+lists = [[1, 2], [3, 4], [5]]
+flat = [item for sublist in lists for item in sublist]
+print(flat)  
+
